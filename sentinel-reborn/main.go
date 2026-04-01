0011361aeaaac8d89736844c8eb8185c7d3c43ab
Cosmic Code Archer <gackmar@outlook.com>
2026-04-01 20:32:10 +0100

    sentinel-reborn

--- sentinel-reborn/main.go
+++ sentinel-reborn/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"os"
+	"strings"
+
+	"sentinel-reborn/theInterface"
+)
+
+func pipe(s string) string {
+	s = theInterface.BinToDec(s)
+	s = theInterface.ReplaceHexWithDec(s)
+	s = theInterface.Up(s)
+	if strings.Contains(s, "(cap,") {
+		s = theInterface.CapN(s)
+	}
+	if strings.Contains(s, "(low,") {
+		s = theInterface.LowN(s)
+	}
+	s = theInterface.UpN(s)
+	s = theInterface.Punctuations(s)
+	s = theInterface.FixQuotes(s)
+	s = theInterface.Vol(s)
+	return s
+}
+
+func main() {
+	in, _ := os.ReadFile("input.txt")
+	lines := strings.Split(strings.ReplaceAll(string(in), "\r\n", "\n"), "\n")
+	for i := range lines {
+		lines[i] = pipe(lines[i])
+	}
+	os.WriteFile("output.txt", []byte(strings.Join(lines, "\n")), 0644)
+}
