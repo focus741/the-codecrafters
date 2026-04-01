c2fba75b912dd63ee91366a9906a64229a62b9d5
oche02 <augustineoche02@gmail.com>
2026-04-01 14:56:52 +0100

    i completed my cap n project

---
+++ sentinel-reborn/capn.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println(cap("i remember my name, like yesterday (cap, 6)"))
+}
+
+func cap(s string) string {
+	open_Tag := strings.Index(s, "(")
+	close_Tag := strings.Index(s, ")")
+	Tag := s[open_Tag : close_Tag+1]
+
+	beforeTag := s[:open_Tag]
+	words := strings.Fields(beforeTag)
+
+	n := 0
+	for i := 0; i < len(Tag)-1; i++ {
+		if strings.ContainsAny(string(Tag[i]), "123456789") {
+			n, _ = strconv.Atoi(string(Tag[i]))
+		}
+	}
+
+	for i := len(words) - n; i < len(words); i++ {
+		words[i] = strings.Title(words[i])
+	}
+	words = append(words, s[close_Tag+1:])
+
+	return strings.Join(words, " ")
+}
